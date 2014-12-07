a14152168c121e42101336060147c6face57eeca
erbao.wang <erbao.wang@renren-inc.com>
2014-12-07 23:19:21 +0800

    实现线程范围内的共享变量

---
+++ src/com/web/thread/ThreadLocalTest.java
@@ -0,0 +1,63 @@
+package com.web.thread;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * 
+
+ * @ClassName: ThreadLocalTest
+
+ * @Description: 实现线程范围内的共享变量
+ * 
+ * @see ThreadLocal
+
+ * @author: web
+
+ * @date: 2014年12月7日 下午6:09:45
+ */
+public class ThreadLocalTest {
+
+	static Map<Thread,Integer> threadData=new HashMap<Thread,Integer>();
+	
+	public static void main(String[] args) {
+		for(int i=1;i<=2;i++){
+			new ThreadLocalTest().init();
+		}
+
+	}
+
+	private void init(){
+		int data=new Random().nextInt();
+		new Thread(
+				new Runnable(){
+					public void run() {
+						threadData.put(Thread.currentThread(),data);
+						System.out.println(Thread.currentThread().getName()+"put data= " +data);
+						new A().get();//A  中只获取线程1中的数据
+						new B().get();//B  中只获取线程2中的数据
+					};
+				}
+	    ).start();
+	}
+	
+	static class A{
+		
+		public void get(){
+			int data=threadData.get(Thread.currentThread());
+			System.out.println("Data form A "+Thread.currentThread()+"data :"+data);
+		}
+	}
+
+	static class B{
+		
+		public void get(){
+			int data=threadData.get(Thread.currentThread());
+			System.out.println("Data form B "+Thread.currentThread()+"data :"+data);
+		}
+	}
+}
+
+
+
