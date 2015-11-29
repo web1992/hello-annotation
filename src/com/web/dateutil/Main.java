edf443b4a741f62046753d765ac27d6d0471990f
web1992 <554933654@qq.com>
2015-11-29 22:10:46 +0800

    日期测试add

--- src/com/web/dateutil/Main.java
+++ src/com/web/dateutil/Main.java
@@ -12,38 +12,8 @@ import static com.web.util.Util.print;
 public class Main {
 
     public static void main(String[] a) {
-//        long day=1000*60*60*24;
-//
-//        Date _data=new Date();
-//        System.out.println(_data);
-//        //日期：2009年3月11号
-//        Calendar c1 = Calendar.getInstance();
-//        c1.set(2015, 12 - 1, 31);
-//        System.out.println("WEEK_OF_YEAR=" + c1.get(Calendar.WEEK_OF_YEAR));
-//        System.out.println(c1.getTime());
-//        System.out.println("----");
-//        Calendar c12 = Calendar.getInstance();
-//        c12.set(2016, 1 - 1, 1);
-//        System.out.println("WEEK_OF_YEAR=" + c12.get(Calendar.WEEK_OF_YEAR));
-//
-//        System.out.println(c12.getTime());
-//
-//        Calendar cal = new GregorianCalendar();
-//        System.out.println(cal.getTimeZone());
-//        // 获取游戏工具类的时区
-//        cal.setTimeZone(TimeUtils.TIME_ZONE);
-//        System.out.println(cal.getTimeZone());
-//        int weekOfYear = cal.get(Calendar.WEEK_OF_YEAR);
-//        System.out.println(weekOfYear);
-//        System.out.println(cal.getFirstDayOfWeek());
-//        System.out.println("WEEK_OF_YEAR=" + cal.get(Calendar.WEEK_OF_YEAR));
-//
-//        //
-//        Calendar c1 = Calendar.getInstance();
-//        c1.set(2015, 12 - 1, 31);
-//        print("2015, 12 - 1, 31");
-//        print(c1);
 
+        // 打印12月的日期
         int year=2015;
         int month=12;
         int date=31;
@@ -61,17 +31,18 @@ public class Main {
 
         //
 
-//        WEEK_OF_YEAR=1 weekFirstDay=1 date=2015-12-26
-//        WEEK_OF_YEAR=52 weekFirstDay=1 date=2015-12-26
+//       WEEK_OF_YEAR=1 weekFirstDay=1 date=2015-12-27
+//       WEEK_OF_YEAR=52 weekFirstDay=1 date=2015-12-26
+        // 测试日期是否是同一周
         Calendar st = Calendar.getInstance();
         st.setTimeZone(TimeUtils.TIME_ZONE);
-        st.set(2015, 12, 25);
+        st.set(2015, 12, 26);
         st.setFirstDayOfWeek(Calendar.MONDAY);
 
         Calendar et = Calendar.getInstance();
         et.setTimeZone(TimeUtils.TIME_ZONE);
         et.setFirstDayOfWeek(Calendar.MONDAY);
-        et.set(2015, 12, 23);
+        et.set(2015, 12, 27);
 
         if (st.get(Calendar.WEEK_OF_YEAR) == et.get(Calendar.WEEK_OF_YEAR)) {
             print("true");
