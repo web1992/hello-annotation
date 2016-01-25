8a96f0318e61479f68fd7e1572c824c37455c008
web1992 <55493654@qq.com>
2016-01-25 14:04:27 +0800

    hash map null  值测试

--- src/com/web/util/Util.java
+++ src/com/web/util/Util.java
@@ -1,12 +1,31 @@
 package com.web.util;
 
+import java.util.HashMap;
+import java.util.Set;
+import java.util.UUID;
+
 /**
  * Created by erniu on 2015/11/29.
  */
 public class Util {
 
-    public static void print(Object args){
+    public static void print(Object args) {
         System.out.println(args);
         System.out.println("\n");
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        final HashMap<String, String> map = new HashMap<String, String>(2);
+
+        map.put(null,"1");
+
+        System.out.println(map.get(null));
+
+        map.put(null,"2");
+
+        System.out.println(map.get(null));
     }
 }
